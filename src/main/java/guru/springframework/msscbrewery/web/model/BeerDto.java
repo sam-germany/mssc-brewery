50b4003882c02bff1fb6e410ef8ef0712e679dd2
sunny <sunny.keila24@gmail.com>
2020-11-23 18:49:33 +0100

    project created  23.11.20

---
+++ src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,10 @@
+package guru.springframework.msscbrewery.web.model;
+
+import java.util.UUID;
+
+public class BeerDto {
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    
+}
